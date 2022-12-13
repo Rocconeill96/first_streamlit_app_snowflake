14d8ec1bcc8e2564b1154f9835277b50c5b481ca
Rocconeill96 <116355407+Rocconeill96@users.noreply.github.com>
2022-12-13 17:55:20 +0000

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit 
 
 streamlit.title('Snowflake streamlit application')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boilded Free-Range Egg')
+
+
