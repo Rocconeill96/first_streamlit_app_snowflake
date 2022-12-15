431313539ef7daa05f1067b842b8e94bf7a9d66c
Rocconeill96 <116355407+Rocconeill96@users.noreply.github.com>
2022-12-15 17:59:26 +0000

    Update streamlit_app.py
    redistributed multiple lines of code into another function 
    1)function that queries the table
    2)a button that calls the function and loads data into page

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,30 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 except URLerror as e:
    streamlit.error()
- 
+    
+streamlit.header("The fruit load list contains:")
+#snowflake functions 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute('select * from fruit_load_list')
+    return my_cur.fetchall()
+  
+#add button to load fruit 
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
-
-#don't run while troubleshooting
+# #don't run while troubleshooting
 streamlit.stop()
 
 # import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select * from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
 
 #allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
