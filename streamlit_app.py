1b85e1354d938981598361dddfee766f9b661cc7
Rocconeill96 <116355407+Rocconeill96@users.noreply.github.com>
2022-12-15 17:24:54 +0000

    Update streamlit_app.py
    rearranged libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
+#all libraries 
 import streamlit 
+import pandas as pd 
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Snowflake streamlit application')
 
@@ -10,7 +15,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas as pd 
+# import pandas as pd 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +31,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+ fruit_choice)
 # streamlit.text(fruityvice_response)
 
@@ -39,7 +44,7 @@ streamlit.dataframe(fruityvice_normalized)
 #don't run while troubleshooting
 streamlit.stop()
 
-import snowflake.connector
+# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
